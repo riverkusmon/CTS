6aa619e69e852f62e0ccaccd98be850536942b96
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 16:02:45 +0000

    implmented destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,9 +37,16 @@ class PortfoliosController < ApplicationController
   end
 def show
  @portfolio_item = Portfolio.find(params[:id])
-
-
 end
-
+    def destroy
+   #perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+    #destroy the record
+    @portfolio_item.destroy
+   #redirect 
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'record was removed' }
+    end
+    end
 
 end
