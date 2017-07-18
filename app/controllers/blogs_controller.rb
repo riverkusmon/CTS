3d3ef6110c5cb1746383df7fda464b73a7c32c3e
Jordan Hudgens <jordan@devcamp.com>
2017-07-18 15:55:27 +0000

    implemented partials for blog, font awesome, and pagination.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,19 +1,20 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
-layout "blog"
-access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
+  layout "blog"
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
+
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.page(params[:page]).per(5)
     @page_title = "My Portfolio Blog"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-  @page_title = @blog.title
-  @seo_keywords = @blog.body
+    @page_title = @blog.title
+    @seo_keywords = @blog.body
   end
 
   # GET /blogs/new
@@ -32,11 +33,9 @@ access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
+        format.html { redirect_to @blog, notice: 'Your post is now live.' }
       else
         format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -47,10 +46,8 @@ access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :
     respond_to do |format|
       if @blog.update(blog_params)
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -60,18 +57,21 @@ access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'Post was removed.' }
       format.json { head :no_content }
     end
   end
-def toggle_status
-  if @blog.draft?
-    @blog.published!
-  elsif @blog.published?
-  @blog.draft!
+
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+        
+    redirect_to blogs_url, notice: 'Post status has been updated.'
   end
-  redirect_to blogs_url, notice: 'post status has been updated'
-end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
