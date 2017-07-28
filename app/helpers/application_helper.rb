1241f8420d63aaaf796ea5e551ce5322df98e45d
Jordan Hudgens <jordan@devcamp.com>
2017-07-28 23:02:35 +0000

    integrated twitter gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+    DevcampViewTool::Renderer.copyright 'Andrew Gardner', 'All rights reserved'
   end
 
   def nav_items
