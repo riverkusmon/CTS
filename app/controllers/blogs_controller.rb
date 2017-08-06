cd246b3a5d11580fc93a041feca7a65d73d96a63
Jordan Hudgens <jordan@devcamp.com>
2017-08-06 03:09:43 +0000

    added validations for topics in blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,3 @@
-
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
@@ -10,22 +9,22 @@ class BlogsController < ApplicationController
     if logged_in?(:site_admin)
       @blogs = Blog.recent.page(params[:page]).per(5)
     else
-      @blogs = Blog.published.page(params[:page]).per(5)
+      @blogs = Blog.published.recent.page(params[:page]).per(5)
     end
-      @page_title = "My Portfolio Blog"
+    @page_title = "My Portfolio Blog"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
     if logged_in?(:site_admin) || @blog.published?
-    @blog = Blog.includes(:comments).friendly.find(params[:id])
-    @comment = Comment.new
+      @blog = Blog.includes(:comments).friendly.find(params[:id])
+      @comment = Comment.new
 
-    @page_title = @blog.title
-    @seo_keywords = @blog.body
+      @page_title = @blog.title
+      @seo_keywords = @blog.body
     else
-      redirect_to blogs_path, notice: "youa are not authorized to access this page"
+      redirect_to blogs_path, notice: "You are not authorized to access this page"
     end
   end
 
@@ -92,6 +91,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body)
+      params.require(:blog).permit(:title, :body, :topic_id)
     end
 end
